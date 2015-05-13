cd4ea00133ca919477c92933394de1c6681c56fd
hansondg <danielghanson93@gmail.com>
2015-05-13 00:58:18 -0400

    added nigaction and levelmanager classes for level progression

--- Assets/Scripts/Action/UpdateAction.cs
+++ Assets/Scripts/Action/UpdateAction.cs
@@ -3,12 +3,41 @@ using UnityEngine;
 
 namespace Update.Action {
 
+	// generic action that can be triggerred by dialogue
+	[System.Serializable]
 	public abstract class UpdateAction : MonoBehaviour {
 
 		public string actionName;
 
-		// add stuff here to add action to level
-		// so that the level remains changed
 		public abstract void Apply();
+	}
+
+	// generic Action that is inteded to changes the map
+	[System.Serializable]
+	public abstract class BigAction : UpdateAction {
+
+		public int Level {
+			get;
+			internal set;
+		}
+
+		private bool saved;
+
+		public override void Apply(){
+			if(!saved){
+				LevelManager.RecordAction(this);
+				saved = true;
+			}
+		}
+
+		public void Awake(){
+			saved = false;
+			DontDestroyOnLoad(this);
+		}
+
+		public void OnLevelWasLoaded(int level){
+			if(!saved)
+				this.Level = level;
+		}
 	}
 }
